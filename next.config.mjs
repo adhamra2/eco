7b2b5369d3145a2deb266229668ebdd140ba7ad3
unknown <adhamr1b@gmail.com>
2024-03-01 20:32:39 +0200

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images:{
+        domains:['https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/clt8n4l4u1fdk07utec7duj0z/master/upload']
+    }
+};
 
 export default nextConfig;
